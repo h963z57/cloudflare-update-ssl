fcb0f961f46474d36d52ac9232081b55391f12b4
h963z57 <111640710+h963z57@users.noreply.github.com>
2022-12-11 13:16:41 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ echo "Create SSL"
 certbot certonly --dns-cloudflare --dns-cloudflare-credentials /root/cloudflareapi.cfg -d *.$DOMAIN_NAME -d $DOMAIN_NAME -m $HOSTMASTER_EMAIL --non-interactive --agree-tos
 
 echo "Connect to S3"
-./mc alias set s3 $S3_ADDRESS $S3_LOGIN "'$3_PASSWORD'"
+./mc alias set s3 $S3_ADDRESS $S3_LOGIN $S3_PASSWORD
 
 echo "Copy files to s3"
-./mc cp /etc/letsencrypt/live/h963z57.com/fullchain.pem s3/$S3_PATH/fullchain.pem
-./mc cp /etc/letsencrypt/live/h963z57.com/privkey.pem s3/$S3_PATH/privkey.pem
+./mc cp /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem s3/$S3_PATH/fullchain.pem
+./mc cp /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem s3/$S3_PATH/privkey.pem
